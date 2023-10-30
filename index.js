aa37faaa316c99a7893ee4f700d17acfd2e60691
Taylor Beseda <tbeseda@gmail.com>
2023-10-29 18:40:47 -0600

    update deps and new lint

--- index.js
+++ index.js
@@ -1,90 +1,90 @@
-import FindMyWay from "find-my-way";
+import FindMyWay from 'find-my-way'
 
 export class ObeliskRouter {
-	defaultRoute;
-	router;
-	handlers = new Map();
+  defaultRoute
+  router
+  handlers = new Map()
 
-	constructor({ defaultRoute }) {
-		this.defaultRoute = defaultRoute;
-		this.router = FindMyWay({
-			ignoreTrailingSlash: true,
-			caseSensitive: false,
-		});
-	}
+  constructor ({ defaultRoute }) {
+    this.defaultRoute = defaultRoute
+    this.router = FindMyWay({
+      ignoreTrailingSlash: true,
+      caseSensitive: false,
+    })
+  }
 
-	prettyPrint() {
-		return this.router.prettyPrint();
-	}
+  prettyPrint () {
+    return this.router.prettyPrint()
+  }
 
-	// handle find-my-way ordered args
-	on(method, path, callbackOrOptions, callback, store) {
-		let handler = callback;
-		let options = callbackOrOptions;
+  // handle find-my-way ordered args
+  on (method, path, callbackOrOptions, callback, store) {
+    let handler = callback
+    let options = callbackOrOptions
 
-		if (!callback) {
-			handler = callbackOrOptions;
-			options = {};
-		}
+    if (!callback) {
+      handler = callbackOrOptions
+      options = {}
+    }
 
-		// add pointer method to internal find-my-way router
-		const routeKey = { method, path, options };
-		this.handlers.set(routeKey, handler);
-		this.router.on(method, path, options, () => routeKey, store);
+    // add pointer method to internal find-my-way router
+    const routeKey = { method, path, options }
+    this.handlers.set(routeKey, handler)
+    this.router.on(method, path, options, () => routeKey, store)
 
-		return routeKey;
-	}
+    return routeKey
+  }
 
-	/**
-	 * @description Mounts the router to a Lambda handler
-	 * @returns {import("aws-lambda").Handler}
-	 */
-	mount() {
-		/**
-		 * @description The mounted Lambda handler
-		 * @param {import("aws-lambda").APIGatewayProxyEventV2} event
-		 * @param {import("aws-lambda").Context} context
-		 */
-		return async (event, context) => {
-			const {
-				rawPath,
-				rawQueryString,
-				queryStringParameters,
-				requestContext: { http: reqHttp },
-			} = event;
+  /**
+   * @description Mounts the router to a Lambda handler
+   * @returns {import("aws-lambda").Handler}
+   */
+  mount () {
+    /**
+     * @description The mounted Lambda handler
+     * @param {import("aws-lambda").APIGatewayProxyEventV2} event
+     * @param {import("aws-lambda").Context} context
+     */
+    return async (event, context) => {
+      const {
+        rawPath,
+        rawQueryString,
+        queryStringParameters,
+        requestContext: { http: reqHttp },
+      } = event
 
-			const method = /** @type {FindMyWay.HTTPMethod} */ (reqHttp.method);
+      const method = /** @type {FindMyWay.HTTPMethod} */ (reqHttp.method)
 
-			let payload = { event, context };
+      let payload = { event, context }
 
-			let originalPath = rawPath;
-			if (queryStringParameters) originalPath += `?${rawQueryString}`;
+      let originalPath = rawPath
+      if (queryStringParameters) originalPath += `?${rawQueryString}`
 
-			// look for matching route without invoking it
-			const found = this.router.find(method, originalPath /* constraints */);
+      // look for matching route without invoking it
+      const found = this.router.find(method, originalPath /* constraints */)
 
-			if (found?.handler) {
-				// @ts-ignore handler isn't "real" handler, just a pointer
-				const routeKey = found.handler();
-				const handler = this.handlers.get(routeKey);
-				const { params, searchParams, store } = found;
-				payload = {
-					...payload,
-					params,
-					searchParams,
-					store,
-				};
+      if (found?.handler) {
+        // @ts-ignore handler isn't "real" handler, just a pointer
+        const routeKey = found.handler()
+        const handler = this.handlers.get(routeKey)
+        const { params, searchParams, store } = found
+        payload = {
+          ...payload,
+          params,
+          searchParams,
+          store,
+        }
 
-				const returnResult = await handler(payload);
+        const returnResult = await handler(payload)
 
-				if (returnResult) {
-					return returnResult;
-				} else {
-					return this.defaultRoute(payload);
-				}
-			} else {
-				return this.defaultRoute(payload);
-			}
-		};
-	}
+        if (returnResult) {
+          return returnResult
+        } else {
+          return this.defaultRoute(payload)
+        }
+      } else {
+        return this.defaultRoute(payload)
+      }
+    }
+  }
 }
--- test/mocks.js
+++ test/mocks.js
@@ -5,50 +5,50 @@
 
 /** @type {Context} */
 const context = {
-	awsRequestId: "mock-aws-request-id",
-	functionName: "mock-function-name",
-	functionVersion: "mock-function-version",
-	invokedFunctionArn: "mock-invoked-function-arn",
-	logGroupName: "mock-log-group-name",
-	logStreamName: "mock-log-stream-name",
-	memoryLimitInMB: "128",
-	callbackWaitsForEmptyEventLoop: true,
-	getRemainingTimeInMillis: () => 1000,
-	done: () => {},
-	fail: () => {},
-	succeed: () => {},
-};
+  awsRequestId: 'mock-aws-request-id',
+  functionName: 'mock-function-name',
+  functionVersion: 'mock-function-version',
+  invokedFunctionArn: 'mock-invoked-function-arn',
+  logGroupName: 'mock-log-group-name',
+  logStreamName: 'mock-log-stream-name',
+  memoryLimitInMB: '128',
+  callbackWaitsForEmptyEventLoop: true,
+  getRemainingTimeInMillis: () => 1000,
+  done: () => {},
+  fail: () => {},
+  succeed: () => {},
+}
 
 /** @type {Event} */
 const event = {
-	version: "2.0",
-	rawPath: "/my/path",
-	rawQueryString: "parameter1=value1&parameter1=value2&parameter2=value",
-	headers: {
-		header1: "value1",
-		header2: "value1,value2",
-	},
-	queryStringParameters: {
-		parameter1: "value1,value2",
-		parameter2: "value",
-	},
-	pathParameters: { parameter1: "value1" },
-	requestContext: {
-		// @ts-ignore
-		http: {
-			method: "GET",
-		},
-	},
-};
+  version: '2.0',
+  rawPath: '/my/path',
+  rawQueryString: 'parameter1=value1&parameter1=value2&parameter2=value',
+  headers: {
+    header1: 'value1',
+    header2: 'value1,value2',
+  },
+  queryStringParameters: {
+    parameter1: 'value1,value2',
+    parameter2: 'value',
+  },
+  pathParameters: { parameter1: 'value1' },
+  requestContext: {
+    // @ts-ignore
+    http: {
+      method: 'GET',
+    },
+  },
+}
 
 /** @type {Event} */
 const rootRequest = {
-	...event,
-	rawPath: "/",
-	rawQueryString: "",
-	headers: {},
-	queryStringParameters: {},
-	pathParameters: {},
-};
+  ...event,
+  rawPath: '/',
+  rawQueryString: '',
+  headers: {},
+  queryStringParameters: {},
+  pathParameters: {},
+}
 
-export { event, rootRequest, context };
+export { event, rootRequest, context }
